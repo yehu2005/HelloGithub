031f0e03f149a3ed1e3147a547e9fc2225cc4901
yehu2005 <yehu_2005@163.com>
2017-04-12 17:13:24 +0800

    Initial commit

---
+++ src/main/java/com/yehu/start/JMain.java
@@ -0,0 +1,12 @@
+package com.yehu.start;
+
+/**
+ * Created by YeHu on 2017/4/12.
+ */
+public class JMain {
+
+    public static void main(String[] args){
+        System.out.println("Hello Github");
+    }
+
+}
